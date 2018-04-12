a014a7deb962d57f87eca6c409b996c5c9b475bb
db0company <db0company@gmail.com>
2018-04-12 12:28:14 +0800

    Only show banners up to 5 days in the future

--- bang/management/commands/generate_settings.py
+++ bang/management/commands/generate_settings.py
@@ -18,8 +18,12 @@ def generate_settings():
     print 'Get the latest news'
     now = timezone.now()
     two_days_ago = now - datetime.timedelta(days=2)
+    in_five_days = now + datetime.timedelta(days=5)
     for version in models.Account.VERSIONS.values():
-        filters = { version['prefix'] + 'end_date__gte': two_days_ago }
+        filters = {
+            version['prefix'] + 'end_date__gte': two_days_ago,
+            version['prefix'] + 'end_date__lte': in_five_days,
+        }
         old_lang = get_language()
         for event in (list(models.Event.objects.filter(**filters))
                       + list(models.Gacha.objects.filter(**filters))):

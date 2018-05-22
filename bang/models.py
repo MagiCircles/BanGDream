afda562be2bf8ebff8233d4a504df97ed87061f2
karin <summertriangle.dev@gmail.com>
2018-05-22 00:36:30 -0700

    remove live2d_model_pkg from Card

--- bang/models.py
+++ bang/models.py
@@ -548,19 +548,6 @@ class Card(MagiModel):
     _2x_transparent_trained = models.ImageField(null=True)
     transparent_trained = models.ImageField(string_concat(_('Transparent'), ' (', _('Trained'), ')'), upload_to=uploadItem('c/transparent/a'), null=True)
 
-    # Live2D
-    live2d_model_pkg = models.FileField('Live2D', upload_to=uploadItem('c/l2d'), null=True)
-    _tthumbnail_live2d_screenshot = models.ImageField(null=True)
-    live2d_screenshot = models.ImageField(upload_to=uploadItem('c/l2d/s'), null=True)
-
-    @property
-    def live2d_url(self):
-        return u'/live2d/{}/{}/'.format(self.id, tourldash(unicode(self)))
-
-    @property
-    def ajax_live2d_url(self):
-        return u'/ajax/live2d/{}/'.format(self.id)
-
     # Statistics
     performance_min = models.PositiveIntegerField(string_concat(_('Performance'), ' (', _('Minimum'), ')'), default=0)
     performance_max = models.PositiveIntegerField(string_concat(_('Performance'), ' (', _('Maximum'), ')'), default=0)

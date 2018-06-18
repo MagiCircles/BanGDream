d7de7232547041312c8a3375360533e48358f20e
karin <summertriangle.dev@gmail.com>
2018-06-17 18:02:37 -0700

    fix server behaviour for cameos checkbox
    broken in f5b5624

--- bang/forms.py
+++ bang/forms.py
@@ -229,13 +229,14 @@ class CardFilterForm(MagiFiltersForm):
         return queryset.filter(Q(i_skill_type=value) | Q(i_side_skill_type=value))
     i_skill_type_filter = MagiFilter(to_queryset=skill_filter_to_queryset)
 
-    def member_id_to_queryset(self, queryset, request, value):
+    def member_to_queryset(self, queryset, request, value):
         if self.data.get('member_includes_cameos'):
             return queryset.filter(Q(member_id=value) | Q(cameo_members__id=value))
         else:
             return queryset.filter(member_id=value)
 
     member = forms.ChoiceField(choices=BLANK_CHOICE_DASH + [(id, full_name) for (id, full_name, image) in getattr(django_settings, 'FAVORITE_CHARACTERS', [])], initial=None, label=_('Member'))
+    member_filter = MagiFilter(to_queryset=member_to_queryset)
 
     member_includes_cameos = forms.BooleanField(label=_('Include cameos'))
     # used in member_id_to_queryset

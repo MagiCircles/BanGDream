MagiCircles/BanGDream

commit f5b5624a5c45c118026acd1211c2ba6e41b6d742

Rename member_id -> member in accounts list collectiblecards

bang/forms.py
@@ -81,8 +81,8 @@ class FilterAccounts(MagiFiltersForm):
         ('start_date', _('Start Date')),
     ]
 
-    member_id = forms.ChoiceField(choices=BLANK_CHOICE_DASH + [(id, full_name) for (id, full_name, image) in getattr(django_settings, 'FAVORITE_CHARACTERS', [])], required=False, label=_('Favorite Member'))
-    member_id_filter = MagiFilter(selectors=['owner__preferences__favorite_character{}'.format(i) for i in range(1, 4)])
+    member = forms.ChoiceField(choices=BLANK_CHOICE_DASH + [(id, full_name) for (id, full_name, image) in getattr(django_settings, 'FAVORITE_CHARACTERS', [])], required=False, label=_('Favorite Member'))
+    member_filter = MagiFilter(selectors=['owner__preferences__favorite_character{}'.format(i) for i in range(1, 4)])
 
     has_friend_id = forms.NullBooleanField(required=False, initial=None, label=_('Friend ID'))
     has_friend_id_filter = MagiFilter(selector='friend_id__isnull')
@@ -95,7 +95,7 @@ class FilterAccounts(MagiFiltersForm):
 
     class Meta(MagiFiltersForm.Meta):
         model = models.Account
-        fields = ('search', 'friend_id', 'i_version', 'i_color', 'member_id', 'has_friend_id')
+        fields = ('search', 'friend_id', 'i_version', 'i_color', 'member', 'has_friend_id')
 
 ############################################################
 # Member
@@ -316,8 +316,8 @@ def to_CollectibleCardFilterForm(cls):
             if value == '1': return queryset.filter(card__i_skill_type=value) # Score up
             return queryset.filter(Q(card__i_skill_type=value) | Q(card__i_side_skill_type=value))
 
-        member_id = forms.ChoiceField(choices=BLANK_CHOICE_DASH + [(id, full_name) for (id, full_name, image) in getattr(django_settings, 'FAVORITE_CHARACTERS', [])], initial=None, label=_('Member'))
-        member_id_filter = MagiFilter(selector='card__member_id')
+        member = forms.ChoiceField(choices=BLANK_CHOICE_DASH + [(id, full_name) for (id, full_name, image) in getattr(django_settings, 'FAVORITE_CHARACTERS', [])], initial=None, label=_('Member'))
+        member_filter = MagiFilter(selector='card__member_id')
 
         member_band = forms.ChoiceField(choices=BLANK_CHOICE_DASH + i_choices(models.Member.BAND_CHOICES), initial=None, label=_('Band'))
         member_band_filter = MagiFilter(selector='card__member__i_band')
@@ -334,7 +334,7 @@ def to_CollectibleCardFilterForm(cls):
 
         class Meta(cls.ListView.filter_form.Meta):
             pass
-            fields = ('search', 'member_id', 'member_band', 'i_rarity', 'i_attribute', 'i_skill_type', 'member_band', 'ordering', 'reverse_order', 'view')
+            fields = ('search', 'member', 'member_band', 'i_rarity', 'i_attribute', 'i_skill_type', 'member_band', 'ordering', 'reverse_order', 'view')
     return _CollectibleCardFilterForm
 
 ############################################################

bang/magicollections.py
@@ -48,7 +48,7 @@ class AccountCollection(_AccountCollection):
     _version_images = [_c['image'] for _c in models.Account.VERSIONS.values()]
     _play_with_icons = [_c['icon'] for _c in models.Account.PLAY_WITH.values()]
     filter_cuteform = {
-        'member_id': {
+        'member': {
             'to_cuteform': lambda k, v: FAVORITE_CHARACTERS_IMAGES[k],
             'title': _('Member'),
             'extra_settings': {

commit d7de7232547041312c8a3375360533e48358f20e

fix server behaviour for cameos checkbox
broken in f5b5624

bang/forms.py
@@ -229,13 +229,14 @@ class CardFilterForm(MagiFiltersForm):
         return queryset.filter(Q(i_skill_type=value) | Q(i_side_skill_type=value))
     i_skill_type_filter = MagiFilter(to_queryset=skill_filter_to_queryset)
 
-    def member_id_to_queryset(self, queryset, request, value):
+    def member_to_queryset(self, queryset, request, value):
         if self.data.get('member_includes_cameos'):
             return queryset.filter(Q(member_id=value) | Q(cameo_members__id=value))
         else:
             return queryset.filter(member_id=value)
 
     member = forms.ChoiceField(choices=BLANK_CHOICE_DASH + [(id, full_name) for (id, full_name, image) in getattr(django_settings, 'FAVORITE_CHARACTERS', [])], initial=None, label=_('Member'))
+    member_filter = MagiFilter(to_queryset=member_to_queryset)
 
     member_includes_cameos = forms.BooleanField(label=_('Include cameos'))
     # used in member_id_to_queryset
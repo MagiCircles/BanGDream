b204fb7334a24924315a964bce88618a50c39649
LatidoReMe <30684769+LatidoReMe@users.noreply.github.com>
2018-06-26 23:34:23 -0700

    squashed migrations

--- bang/migrations/0044_auto_20180626_2333.py
+++ bang/migrations/0044_auto_20180626_2333.py
@@ -2,15 +2,22 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import magi.utils
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('bang', '0045_auto_20180620_1436'),
+        ('bang', '0043_auto_20180620_1432'),
     ]
 
     operations = [
+        migrations.AddField(
+            model_name='member',
+            name='classroom',
+            field=models.CharField(max_length=10, null=True, verbose_name='Classroom'),
+            preserve_default=True,
+        ),
         migrations.AddField(
             model_name='member',
             name='height',

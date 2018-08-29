28f1e39bfe95617a4ceb96f528584c07a274da43
karin <summertriangle.dev@gmail.com>
2018-08-29 02:50:26 -0700

    goodnight old chibis

--- bang/models.py
+++ bang/models.py
@@ -626,10 +626,6 @@ class Card(MagiModel):
     def overall_trained_max(self):
         return self.performance_trained_max + self.technique_trained_max + self.visual_trained_max
 
-    # Chibi images
-
-    chibis = models.ManyToManyField(Image, related_name="chibi", verbose_name=_('Chibi'))
-
     # Other members that appear in the card art
     cameo_members = models.ManyToManyField(Member, related_name='cameo_members', verbose_name=_('Cameos'))
 
@@ -767,38 +763,6 @@ class Card(MagiModel):
                 'image': unicode(gacha.image),
             })
         return gachas if gachas else None
-
-    # Cache chibis
-
-    _cache_chibis_days = 200
-    _cache_chibis_last_update = models.DateTimeField(null=True)
-    _cache_chibis_ids = models.TextField(null=True)
-    _cache_chibis_paths = models.TextField(null=True)
-
-    def update_cache_chibis(self, chibis=None):
-        self._cache_chibis_last_update = timezone.now()
-        if not chibis:
-            chibis = self.chibis.all()
-        self._cache_chibis_ids = join_data(*[ image.id for image in chibis ])
-        self._cache_chibis_paths = join_data(*[ unicode(image) for image in chibis ])
-
-    def force_cache_chibis(self):
-        self.update_cache_chibis()
-        self.save()
-
-    @property
-    def cached_chibis(self):
-        if not self._cache_chibis_last_update or self._cache_chibis_last_update < timezone.now() - datetime.timedelta(days=self._cache_chibis_days):
-            self.force_cache_chibis()
-        if not self._cache_chibis_ids:
-            return []
-        return [AttrDict({
-            'id': id,
-            'pk': id,
-            'image': path,
-            'image_url': get_image_url_from_path(path),
-            'http_image_url': get_http_image_url_from_path(path),
-        }) for id, path in zip(split_data(self._cache_chibis_ids), split_data(self._cache_chibis_paths))]
 
     # Cache cameos
 

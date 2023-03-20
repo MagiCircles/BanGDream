5eeacbb8bbac27de0e6a09b1dda33eb04f7c7d10
db0company <db0company@gmail.com>
2023-03-20 16:07:20 +0100

    Allow cards with higher rarities but only 1 image

--- bang/models.py
+++ bang/models.py
@@ -800,7 +800,7 @@ class Card(MagiModel):
 
     @property
     def trainable(self):
-        return self.i_rarity in self.TRAINABLE_RARITIES
+        return self.i_rarity in self.TRAINABLE_RARITIES and self.image_trained
 
     MAX_LEVELS = {
         1: 20,
